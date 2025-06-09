4334972a5083d8e3edbf9eb505b681c900e5dc7b
rafiqwe <rmlmanik502@gmail.com>
2025-06-09 08:02:52 +0600

    How to use fs appendFile()

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 
-fs.writeFile("output.txt", "Hello World!", (err) => {
+// writeFile function to create or overwrite a file
+// fs.writeFile("output.txt", "Hello World!", (err) => {
+//     if(err) console.error("Error writing file:", err);
+//     else console.log("File written successfully!");
+// })
+
+// appendFile function to add content to an existing file
+fs.appendFile("output.txt", " Hii Bro", (err) => {
     if(err) console.error("Error writing file:", err);
-    else console.log("File written successfully!");
+    else console.log("Content appended successfully!");
 })
