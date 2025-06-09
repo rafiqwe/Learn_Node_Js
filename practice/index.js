d9257cfde7d527f78019c5bfb5a387e18f30c1ab
rafiqwe <rmlmanik502@gmail.com>
2025-06-10 02:47:46 +0600

    Create edit page and create the backend logic

--- practice/index.js
+++ practice/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import fs from "fs";
+import path from "path";
 const app = express();
 const port = 8080;
 
@@ -23,11 +24,50 @@ app.get("/tasks/:fileName", (req, res) => {
         console.error(error);
       } else {
         console.log("File Data :", fileData);
-        res.render("readMore", { fileName: req.params.fileName, fileData: fileData });
+        res.render("readMore", {
+          fileName: req.params.fileName,
+          fileData: fileData,
+        });
       }
     }
   );
 });
+
+app.get("/edit/:fileName", (req, res) => {
+  res.render("edit", { fileName: req.params.fileName });
+});
+
+app.post("/edit", (req, res) => {
+  const newFilePath = `./files/${req.body.newFileName
+    .split(" ")
+    .join("_")}.txt`;
+  const oldFilePath = `./files/${req.body.oldFileName}`;
+  fs.rename(oldFilePath, newFilePath, (err) => {
+    if (err) {
+      console.error("Error creating file:", err);
+      res.status(500).send("Error creating file");
+    } else {
+      console.log("File created successfully");
+      res.redirect("/");
+    }
+  });
+});
+
+
+// I try it but I fail I'll do it 
+// app.delete("/delete/:deleteElm", (req, res) => {
+//   const folderPath = `./files/${req.params.deleteElm}`;
+//   console.log();
+//   fs.rm(folderPath, { recursive: true, force: true }, (err) => {
+//     if (err) {
+//       console.error("Delete failed:", err);
+//       return res.status(500).send("Failed to delete directory.");
+//     } else {
+//       console.log("Deleted successfully");
+//       return redirect("/");
+//     }
+//   });
+// });
 
 app.post("/create", (req, res) => {
   const filePath = `./files/${req.body.taskName.split(" ").join("_")}.txt`;
