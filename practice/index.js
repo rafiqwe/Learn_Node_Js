aa3bf98e200289b56fb6857e113b8e32cc4fbbb1
rafiqwe <rmlmanik502@gmail.com>
2025-06-10 00:59:35 +0600

    Create ejs code. and backend logic to create a file and show the frontend

--- practice/index.js
+++ practice/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import fs from "fs";
 const app = express();
 const port = 8080;
 
@@ -8,18 +9,24 @@ app.use(express.static("public")); // Serve static files from the "public" direc
 app.set("view engine", "ejs"); // Set EJS as the templating engine
 
 app.get("/", (req, res) => {
-  res.render("index");
+  fs.readdir("./files", (err, files) => {
+    res.render("index", { files });
+  });
+});
+app.post("/create", (req, res) => {
+  console.log("Creating file:", req.body.taskName);
+  const filePath = `./files/${req.body.taskName.split(" ").join('_')}.txt`;
+  fs.writeFile(filePath, req.body.taskDescription, (err) => {
+    if (err) {
+      console.error("Error creating file:", err);
+      res.status(500).send("Error creating file");
+    } else {
+      console.log("File created successfully");
+      res.redirect("/");
+    }
+  });
 });
 
-app.get("/profile/:username", (req, res) => {
-  const { username } = req.params;
-  res.send(`Welcome to ${username}'s profile page!`);
-});
-
-app.get("/author/:username/:age", (req, res) => {
-  const { username, age } = req.params;
-  res.send(`Welcome to ${username}'s profile page! They are ${age} years old.`);
-});
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
