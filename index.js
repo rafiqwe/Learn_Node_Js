82bd0db7abcf7108f53c768f05e217ea44ae57bb
rafiqwe <rmlmanik502@gmail.com>
2025-06-09 08:09:40 +0600

    How to use fs rename()

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const fs = require('fs');
 // })
 
 // appendFile function to add content to an existing file
-fs.appendFile("output.txt", " Hii Bro", (err) => {
-    if(err) console.error("Error writing file:", err);
-    else console.log("Content appended successfully!");
+// fs.appendFile("output.txt", " Hii Bro", (err) => {
+//     if(err) console.error("Error writing file:", err);
+//     else console.log("Content appended successfully!");
+// })
+
+// rename function to change the name of a file
+fs.rename("output.txt", "hello.txt", (err) => {
+    if(err) console.error("Error renaming file:", err);
+    else console.log("File renamed successfully!");
 })
